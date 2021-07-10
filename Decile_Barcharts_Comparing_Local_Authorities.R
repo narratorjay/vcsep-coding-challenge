63a4cdca0eca6269280e6e9de72bbad31b99820f
narratorjay <jaywilson@cheerful.com>
2021-07-10 15:42:30 +0100

    looks like graph options will be limited for useability and visibility

--- Decile_Barcharts_Comparing_Local_Authorities.R
+++ Decile_Barcharts_Comparing_Local_Authorities.R
@@ -4,7 +4,7 @@ library(ggplot2)
 setwd("C:\\Users\\Jay\\c3\\redcross")          # remove before uploading
 
 LA_Dep <- read.csv('Edited_Domains_of_Deprivation.csv')
-#names(LA_Dep)
+#   names(LA_Dep) yields
 # [1] "ï..LSOA_code"       "LSOA_name"          "LA_code"           
 # [4] "LA_name"            "IMD_rank"           "IMD_decile"        
 # [7] "Income_rank"        "Income_decile"      "Employed_rank"     
@@ -13,10 +13,15 @@ LA_Dep <- read.csv('Edited_Domains_of_Deprivation.csv')
 #[16] "Crime_decile"       "PoorAccess_rank"    "PoorAccess_decile" 
 #[19] "Environment_rank"   "Environment_decile"
 
-## only use deciles for barchart comparison, keep most even-numbered columns
+# use only deciles for barcharts, remove all odd (and one even-numbered) columns
 LA_Dep <- LA_Dep[, -c(2, seq(1, 19, 2))]
 
+# user will select two values from LA_name to compare deprivation between two areas
+LA_a <- LA_Dep[LA_Dep$LA_name == "Adur", ]
 
+# suggest stick to health for now to keep display clean
+# bring out noise over bias, refer to Daniel Kahneman's book
+# and link to intervention would be effective everywhere
 
 
 
