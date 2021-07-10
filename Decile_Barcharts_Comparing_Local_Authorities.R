9759f96b35596b74c3da8f1f51e0dcd5b756e377
narratorjay <jaywilson@cheerful.com>
2021-07-10 18:21:22 +0100

    improved basic bar chart appearance

--- Decile_Barcharts_Comparing_Local_Authorities.R
+++ Decile_Barcharts_Comparing_Local_Authorities.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(dplyr)
+#library(dplyr)
 library(ggplot2)
 setwd("C:\\Users\\Jay\\c3\\redcross")          # remove before uploading
 
@@ -12,37 +12,20 @@ LA_Dep <- LA_Dep[, -c(2, seq(1, 19, 2))]
 area_A = "Adur"
 area_B = "Birmingham"
 
-#area_A_stats <-  filter(LA_Dep, LA_name == area_A)
-#area_A_DF <- data.frame(area_A_stats)
-#area_A_health <- area_A_stats$Health_decile
-
 area_A_scores <- data.frame(filter(LA_Dep, LA_name == area_A))
 
-#ggplot(area_A_health, aes(area_A_stats$Health_decile)) +
+
 ggplot(area_A_scores, aes(Health_decile)) +
-  geom_histogram(bins=10) + 
+  geom_histogram(bins=10, colour = "#0000CC", fill = "#0000EE60") + 
   coord_flip() + 
-  labs(title = paste("Relative health scores of population in Local Authority District", area_A),
-       x = "Health decile relative all of England, \"1\" is worst",
-       y = "LSOA count  (an LSOA is electoral ward-sized or smaller)" )
-       #theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-      #       panel.background = element_rect(fill = 'white', colour = 'white')))
-
-
-#ggplot(df,aes(x))+geom_histogram(bins=30)+coord_flip()
-
-
-#LA_Dep %>% filter(LA_name == area_A) 
-#LA_a <- LA_Dep[LA_Dep$LA_name == "Adur", ]
+  labs(title = paste("Relative health of population in Local Authority District", area_A),
+       x = "Decile score in England  (higher is better)",
+       y = "LSOA count with that health score" ) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "#0000BB"))
 
 # suggest stick to health for now to keep display clean
 # bring out noise over bias after reading Daniel Kahneman's book
 # and add comments that intervention would likely be effective anywhere
 
-
-#CleanBase <- 
-#  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-#        panel.background = element_rect(fill = 'white', colour = 'white'), 
-#        axis.line = element_line(colour = "white"), legend.position = "none",
-#        axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-#        axis.text.y = element_blank(), axis.ticks.y = element_blank())
