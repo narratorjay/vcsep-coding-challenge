95393a1b8ff5b032a67969d6734603d85dfad71b
narratorjay <jaywilson@cheerful.com>
2021-07-10 17:11:26 +0100

    basic bar chart for single local authority

--- Decile_Barcharts_Comparing_Local_Authorities.R
+++ Decile_Barcharts_Comparing_Local_Authorities.R
@@ -17,11 +17,30 @@ LA_Dep <- read.csv('Edited_Domains_of_Deprivation.csv')
 LA_Dep <- LA_Dep[, -c(2, seq(1, 19, 2))]
 
 # user will select two values from LA_name to compare deprivation between two areas
-LA_a <- LA_Dep[LA_Dep$LA_name == "Adur", ]
+area_A = "Adur"
+area_B = "Birmingham"
+
+area_A_stats <-  filter(LA_Dep, LA_name == area_A)
+area_A_health <- area_A_stats$Health_decile
+
+ggplot(area_A_health, aes(area_A_stats$Health_decile)) +
+  geom_histogram(bins=10) + 
+  coord_flip() + 
+  labs(title = paste("Relative health scores of population in Local Authority District", area_A),
+       x = "Health decile relative all of England, \"1\" is worst", y = "LSOA count  (an LSOA is electoral ward-sized or smaller)")
+
+
+#ggplot(df,aes(x))+geom_histogram(bins=30)+coord_flip()
+
+
+#LA_Dep %>% filter(LA_name == area_A) 
+#LA_a <- LA_Dep[LA_Dep$LA_name == "Adur", ]
 
 # suggest stick to health for now to keep display clean
-# bring out noise over bias, refer to Daniel Kahneman's book
-# and link to intervention would be effective everywhere
+# bring out noise over bias after reading Daniel Kahneman's book
+# and add comments that intervention would likely be effective anywhere
+
+
 
 
 
