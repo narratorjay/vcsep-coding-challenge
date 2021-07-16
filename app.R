ceccef20b16b713afbee763fa40b9b5c2f3096ef
narratorjay <jaywilson@cheerful.com>
2021-07-16 14:16:15 +0100

    updated text

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ ui <- fluidPage(
             choices = LA_List, selected = "Kingston upon Hull, City of"),
     selectInput(inputId = "Second_LA_name", label = "Second local authority", 
             choices = LA_List, selected = "Watford"),
-    helpText("Expecting local authorities across England to display similar distributions of education, health, crime etc, even when ordered by rank rather than scalar values?  Most histograms display similar patterns that fit with my experience of living in Ealing, Manchester, Liverpool, Balham and Winchester over the past four decades.  Many areas (LSOAs) in any local authority are struggling in one way or another, but overall it balances out.  Then I saw the health deciles for Blackpool and could not argue with the RSS feed from RT broadcast on 15th July 2021, \'The UK is the most unequal society in the developed world.\' "),
+    helpText("Are you expecting local authorities across England to display similar distributions of education, health, crime etc, even when ordered by rank rather than scalar values?  Most histograms display similar patterns that fit with my experience of living in Ealing, Manchester, Liverpool, Wandsworth and Winchester over the past four decades.  Many areas (LSOAs) in any local authority are struggling in one way or another, but overall it balances out.  Then I saw the health deciles for Blackpool ... and could not argue with the RSS feed from RT broadcast on 15th July 2021, \'The UK is the most unequal society in the developed world.\'                                 Data downloaded from https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019"),
         ),
   mainPanel(plotOutput("First_Plot"),
             plotOutput("Second_Plot")
@@ -36,20 +36,20 @@ server <- function(input, output) {
                    data = LA_Dep[LA_Dep$LA_name %in% input$First_LA_name,]) +
     coord_flip() +
     labs(title = paste("Relative health of population in local authority district", input$First_LA_name),
-      x = "Decile score in England  (higher is better)",
-      y = "LSOA count with that health score" ) +
+      x = "Decile rank in England  (higher is better)",
+      y = "LSOA count with that rank" ) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
       panel.background = element_rect(fill = 'white', colour = 'white'),
       axis.line = element_line(colour = "#BB0000"))
   })
   output$Second_Plot <- renderPlot({
     ggplot(LA_Dep, aes(x = Health_decile)) +
-      geom_histogram(bins = 10, colour = "#0000CC", fill = "#0000EE60",
+      geom_histogram(bins = 10, colour = "#0000CC", fill = "#00EE0060",
                      data = LA_Dep[LA_Dep$LA_name %in% input$Second_LA_name,]) +
       coord_flip() +
       labs(title = paste("Relative health of population in local authority district", input$Second_LA_name),
-           x = "Decile score in England  (higher is better)",
-           y = "LSOA count with that health score" ) +
+           x = "Decile rank in England  (higher is better)",
+           y = "LSOA count with that rank" ) +
       theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
             panel.background = element_rect(fill = 'white', colour = 'white'),
             axis.line = element_line(colour = "#0000BB"))
